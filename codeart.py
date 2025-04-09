4250d71ae97f722163c134df31803dc502c4918a
JJC1124 <jjcrawford@seattleacademy.org>
2025-04-09 17:44:51 +0000

    editedcodeart

--- codeart.py
+++ codeart.py
@@ -1,10 +1,11 @@
 import turtle
 turtle.Screen().bgcolor("black")
+#turn the background screen black
 t = turtle.Turtle()
 
 t.goto(100, 0)
 t.color("turquoise")
-        
+ #turn color into turquoise       
 
 for i in range( 100 ) :
     t.forward( 120 )
@@ -13,21 +14,25 @@ for i in range( 100 ) :
 for i in range( 75 ) :
     t.forward( 72 )
     t.left(50 + 1)
+    #turn left
 
 t.goto(0, 100)
 t.color("purple")
+#turn the color purple
 for i in range( 100) :
     t.forward( 120)
     t.left(45 + 1)
 
 t.goto(-100, 0)
 t.color("red")
+#turn the color red
 for i in range( 75) :
     t.forward( 60 )
     t.left(75 + 1)
 
 t.goto(0, -100)
 t.color("green")
+#turn the color green
 for i in range( 100) :
     t.forward( 120)
     t.left(45 + 1)
