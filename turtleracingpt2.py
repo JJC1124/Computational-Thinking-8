0e9e2d8735d4b7934d8aa3d2293cc5093afa0959
JJC1124 <jjcrawford@seattleacademy.org>
2025-05-21 18:57:49 +0000

    updated turtle racing project

---
+++ turtleracingpt2.py
@@ -0,0 +1,70 @@
+# Section 1 - Helper functions
+import turtle, time, random
+def set_background(image_filename):
+    screen = turtle.Screen()
+    try:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+    except:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+def create_sprite(image_filename, x=0, y=0):
+    image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite = turtle.Turtle()
+    sprite.shape(image_file)
+    sprite.penup()
+    sprite.goto(x,y)
+    return sprite
+
+
+# Section 2 - Variables
+# TODO - add starting values for all the variables
+x1 = -100
+y1 = 150
+x2 = -100
+y2 = 50
+x3 = -100
+y3 = 0
+x4 = -100
+y4 = -50
+# Section 3 - Setup
+# TODO - use your own background, and set your four turtles to images of your choice
+set_background("castle")
+t1 = create_sprite("basketball",x1,y1)
+t2 = create_sprite("fox",x2,y2)
+t3 = create_sprite("baseball",x3,y3)
+t4 = create_sprite("sodacan",x4,y4)
+
+
+# # Section 4 - Racing
+# # TODO - set how much each variable changes by and increase the number of repeats to at least 30
+# # TODO - explain here which sprites are faster or slower the winner could ether be x1 or x4 but there is a higher chance of it being x4 because it could ether go 5 or 6 steps and that is higher than x1 which could ether go 1 or 6 steps.
+for i in range(30):
+    x1 += random.randint(1,15)
+    x2 += random.randint(2,20)
+    x3 += random.randint(1,12)
+    x4 += random.randint(5,22)
+    t1.goto(x1, y1)
+    t2.goto(x2, y2)
+    t3.goto(x3, y3)
+    t4.goto(x4, y4)
+    time.sleep(0.1)
+
+
+# # Section 5 - Winner
+# # TODO - complete the elif for player 2 winning
+# # TODO - write another elif for player 3 and player 4
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+    print("player 1 wins!")
+elif x2>= x1 and x2>=x3 and x2 >= x4 :
+    print("player 2 wins!")
+elif x3 >= x1 and x3 >= x2 and x3 >= x4:
+    print("player 3 wins!")
+elif x4>= x1 and x4>=x2 and x4 >= x3 :
+    print("player 4 wins!")
+
+
+
+
+
+turtle.exitonclick()
